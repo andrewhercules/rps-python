7df07e7e0413a38939676dc975b03f902ce2908e
Andrew Hercules <andrewhercules@gmail.com>
2017-12-03 14:01:19 +0000

    Added function to set player's name

--- rps/rps.py
+++ rps/rps.py
@@ -2,3 +2,6 @@ class RockPaperScissorsGame:
 
     def __init__(self):
         self.player_name = ''
+
+    def set_player_name(self, name):
+        self.player_name = name
--- rps/tests/test_rps.py
+++ rps/tests/test_rps.py
@@ -9,3 +9,7 @@ new_rps_game = RockPaperScissorsGame()
 
 def test_initial_game_status():
     assert(new_rps_game.player_name == '')
+
+def test_set_player_name():
+    new_rps_game.set_player_name('Player')
+    assert(new_rps_game.player_name == 'Player')
