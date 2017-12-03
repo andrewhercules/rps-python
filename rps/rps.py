a0386b48deb40c8b126e4baa7874f4b04105477d
Andrew Hercules <andrewhercules@gmail.com>
2017-12-03 14:39:49 +0000

    Added function to allow player to select rock, paper, or scissors option

--- rps/rps.py
+++ rps/rps.py
@@ -1,7 +1,15 @@
 class RockPaperScissorsGame:
 
     def __init__(self):
+        self.options = ['rock', 'paper', 'scissors']
         self.player_name = ''
+        self.player_choice = ''
 
     def set_player_name(self, name):
         self.player_name = name
+
+    def set_player_choice(self, player_input_choice):
+        if (player_input_choice in self.options):
+            self.player_choice = player_input_choice
+        else:
+            return "Error!"
--- rps/tests/test_rps.py
+++ rps/tests/test_rps.py
@@ -9,7 +9,23 @@ new_rps_game = RockPaperScissorsGame()
 
 def test_initial_game_status():
     assert(new_rps_game.player_name == '')
+    assert(new_rps_game.options == ['rock', 'paper', 'scissors'])
 
 def test_set_player_name():
     new_rps_game.set_player_name('Player')
     assert(new_rps_game.player_name == 'Player')
+
+def test_set_player_choice_rock():
+    new_rps_game.set_player_choice('rock')
+    assert(new_rps_game.player_choice == 'rock')
+
+def test_set_player_choice_paper():
+    new_rps_game.set_player_choice('paper')
+    assert(new_rps_game.player_choice == 'paper')
+
+def test_set_player_choice_scissors():
+    new_rps_game.set_player_choice('scissors')
+    assert(new_rps_game.player_choice == 'scissors')
+
+def test_set_player_choice_unknown_choice():
+    assert(new_rps_game.set_player_choice('coffee') == "Error!")
