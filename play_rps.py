54e7fbd6817a9e82bc995c666062a780a4605be4
Andrew Hercules <andrewhercules@gmail.com>
2017-12-05 21:27:50 +0000

    Created initial play_rps.py script file

---
+++ play_rps.py
@@ -0,0 +1,7 @@
+from rps.rps import RockPaperScissorsGame
+
+new_game = RockPaperScissorsGame()
+
+player_name = input("Welcome to RPS-Python! \nWhat is your name? ")
+
+print("Your name is: " + player_name)
