f6e007ecb661764f62e677023fd86fea3fca61a0
Andrew Hercules <andrewhercules@gmail.com>
2017-12-06 23:15:38 +0000

    Updated Player class tests to use pytest fixture at test setup

--- player/tests/test_player.py
+++ player/tests/test_player.py
@@ -1,3 +1,4 @@
+import pytest
 import sys
 import os
 myPath = os.path.dirname(os.path.abspath(__file__))
@@ -5,31 +6,30 @@ sys.path.insert(0, myPath + '/../')
 
 from player import Player
 
-def test_initial_player_object_status():
+@pytest.fixture
+def new_player():
     new_player = Player()
+    return new_player
+
+def test_initial_player_object_status(new_player):
     assert(new_player.name == '')
 
-def test_set_player_name():
-    new_player = Player()
+def test_set_player_name(new_player):
     new_player.set_name('Andrew')
     assert(new_player.name == 'Andrew')
 
-def test_set_choice_rock():
-    new_player = Player()
+def test_set_choice_rock(new_player):
     new_player.set_choice('rock')
     assert(new_player.choice == 'rock')
 
-def test_set_choice_paper():
-    new_player = Player()
+def test_set_choice_paper(new_player):
     new_player.set_choice('paper')
     assert(new_player.choice == 'paper')
 
-def test_set_choice_scissors():
-    new_player = Player()
+def test_set_choice_scissors(new_player):
     new_player.set_choice('scissors')
     assert(new_player.choice == 'scissors')
 
-def test_set_choice_unknown_choice():
-    new_player = Player()
+def test_set_choice_unknown_choice(new_player):
     assert(new_player.set_choice('coffee') == 'Error!')
     assert(new_player.choice == '')
