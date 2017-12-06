392acab159766279d5c659810d2660716067c7ba
Andrew Hercules <andrewhercules@gmail.com>
2017-12-06 23:13:03 +0000

    Player object can set choice attribute

--- player/player.py
+++ player/player.py
@@ -2,6 +2,14 @@ class Player:
 
     def __init__(self):
         self.name = ''
+        self.choice = ''
 
     def set_name(self, input_name):
         self.name += input_name
+
+    def set_choice(self, selection):
+        choices = ['rock', 'paper', 'scissors'];
+        if selection in choices:
+            self.choice = selection
+        else:
+            return 'Error!'
--- player/tests/test_player.py
+++ player/tests/test_player.py
@@ -5,11 +5,31 @@ sys.path.insert(0, myPath + '/../')
 
 from player import Player
 
-new_player = Player()
-
 def test_initial_player_object_status():
+    new_player = Player()
     assert(new_player.name == '')
 
 def test_set_player_name():
+    new_player = Player()
     new_player.set_name('Andrew')
     assert(new_player.name == 'Andrew')
+
+def test_set_choice_rock():
+    new_player = Player()
+    new_player.set_choice('rock')
+    assert(new_player.choice == 'rock')
+
+def test_set_choice_paper():
+    new_player = Player()
+    new_player.set_choice('paper')
+    assert(new_player.choice == 'paper')
+
+def test_set_choice_scissors():
+    new_player = Player()
+    new_player.set_choice('scissors')
+    assert(new_player.choice == 'scissors')
+
+def test_set_choice_unknown_choice():
+    new_player = Player()
+    assert(new_player.set_choice('coffee') == 'Error!')
+    assert(new_player.choice == '')
