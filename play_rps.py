f453df0215183e9683670156ca94f7fc32f03ee9
Andrew Hercules <andrewhercules@gmail.com>
2017-12-07 22:01:28 +0000

    Added game logic to ask user to input selection

--- play_rps.py
+++ play_rps.py
@@ -4,4 +4,15 @@ new_game = RockPaperScissorsGame()
 
 player_name = input("Welcome to RPS-Python! \nWhat is your name? ")
 
-print("Your name is: " + player_name)
+new_game.set_player_name(player_name)
+
+print("Hello " + new_game.player_name + ". Welcome to Rock, Paper Scissors")
+
+while(new_game.player_choice == ''):
+    player_option = input("What option would you like to select - rock, paper, or scissors: ")
+    new_game.set_player_choice(player_option)
+    if(new_game.player_choice == ''):
+        print("Error! Input not valid! Please try again!")
+    if(new_game.player_choice != ''):
+        print(new_game.player_name + ", you have selected " + new_game.player_choice)
+        break
